5afd39692edf3234d2a818598dca37d6992f247b
isaismael <ismael.isa02@gmail.com>
2023-03-14 01:11:20 +0100

    se crea page moviedetails

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { LadingPage } from '../pages/LadingPage';
+import { MovieDetails } from '../pages/MovieDetails';
 
 export const MyRoutes = () => {
   return (
     <Router>
         <Routes>
             <Route exact path='/' element={<LadingPage/>} />
+            <Route exact path='/movies/:movieId' element={<MovieDetails/>} />
         </Routes>
     </Router>
   )
