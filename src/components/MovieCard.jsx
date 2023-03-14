e33eaae8b2bc08977ffb3d447f9ffeb99ca4c521
Ismael Isa <ismael.isa02@gmail.com>
2023-03-14 01:15:05 -0300

    se hizo llamada de img, title, genero y descripcion en moviedetail page y se agrego estilos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,16 @@
-import '../components/MovieCard.css'
+import { Link } from 'react-router-dom';
+import styles from '../components/MovieCard.module.css'
 
 export default function MovieCard({movie}) {
 
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
 
   return (
-    <li className='movieCard'>
-      <img src={imageUrl} alt={movie.title} width={230} height={345} className='movieImg'/>
-      <div> {movie.title} </div>
+    <li className={styles.movieCard}>
+      <Link to={'/movies/'+movie.id}> 
+      <img src={imageUrl} alt={movie.title} width={230} height={345} className={styles.movieImg}/>
+      </Link>
+      <div className={styles.title}> {movie.title} </div>
     </li>
   )
 }
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../data/httpClient";
+import { getMoviesImg } from "../utils/getMoviesImg";
+
+import '../pages/MovieDetails.css'
 
 export const MovieDetails = () => {
+  const { movieId } = useParams();
+
+  const [movie, setMovie] = useState([]);
+
+  const [generos, setGeneros] = useState([]);
+
+  const imageUrl = getMoviesImg(movie.poster_path, 500)
+
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data)
+      setGeneros(data.genres[0])
+    });
+  }, [movieId]);
+
   return (
-    <h1>Desde movie details</h1>
-  )
-}
+    <div className="detailsContainer">
+      <img src={imageUrl} alt={movie.title} className= "col movieImage" />
+      <div className="col movieDetails">
+        <p className="title">
+          <strong>Título: </strong>
+          {movie.title}
+        </p>
+        <p>
+          <strong>Género: </strong>
+          {generos.name}
+        </p>
+        <p>
+          <strong>Descripción: </strong>
+          {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
+};
